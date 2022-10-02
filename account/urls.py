abad5fc998ec19d1f3d13a6cda82e30c0ad5540a
Nuno Fernandes <fernandes.n.sa@outlook.fr>
2022-10-03 01:18:48 +0200

    create connexion form ans update name spaces.

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
-    path('/', views.logout_user, name='logout'),
+    path('', views.logout_user, name='logout'),
     path('rgpd/', views.rgpd, name='rgpd'),
     path('meConnecter/', views.connexion, name='connexion'),
     path('m\'inscrire/', views.inscription, name='inscription'),
--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth import logout
 from django.contrib import messages
 from .forms import inscriptionForm
 
@@ -15,7 +16,7 @@ def inscription(request):
             user = form.save()
             login(request, user)
             messages.success(request, f"Nouvelle compte crée: {user.username}")
-            return redirect('account:connexion')
+            return redirect('user:user_home')
         else:
             for error in list(form.errors.values()):
                 messages.error(request, error)
@@ -27,13 +28,35 @@ def inscription(request):
 
 
 def connexion(request):
-    context = {}
-    return render(request, 'account/connexion.html', context)
+    if request.user.is_authenticated:
+        return redirect("user:user_home")
+
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Bienvenue <b>{user.username}</b>! Vous avez été connecté!")
+                return redirect("user:user_home")
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error)
+
+    form = AuthenticationForm()
+
+    context = {"form": form}
+    return render(request, "account/connexion.html", context)
 
 
+@login_required()
 def logout_user(request):
     logout(request)
-    return redirect('home')
+    messages.info(request, "Vous êtes bien déconnecté!")
+    return redirect('account:connexion')
 
 
 def rgpd(request):
