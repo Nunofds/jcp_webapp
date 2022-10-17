00092f4c02ee9cfb745ef8011884c8d4ae0046bf
Nuno Fernandes <fernandes.n.sa@outlook.fr>
2022-10-17 23:45:48 +0200

    config for images files, all is done

--- services/models.py
+++ services/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Services(models.Model):
     name = models.CharField('Nom Service', max_length=100, null=True)
-    image = models.ImageField('Image Service', null=True)
+    image = models.ImageField('Image Service', upload_to='services', blank=True, null=True)
     desc = models.TextField('Description Service', null=True)
     created_at = models.DateTimeField('Date Creation', auto_now_add=True, null=True)
     modified_at = models.DateTimeField('Date Mise Jour', auto_now=True, null=True)
