0cc0a8032052b74e95dc92368111d7e30e893426
Nuno Fernandes <fernandes.n.sa@outlook.fr>
2022-10-17 21:39:53 +0200

    comment roles decorator because don't use

--- account/decorators.py
+++ account/decorators.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect
+from django.http import HttpResponse
 
 
 def user_not_authenticated(function=None):
@@ -23,11 +24,16 @@ def user_not_authenticated(function=None):
     return decorator
 
 
-def allowed_users(allowed_roles=[]):
-    def decorator(view_function):
-        def wrapper_func(request, *args, **kwargs):
-            return view_function(request, *args, **kwargs)
-
-        return wrapper_func
-
-    return decorator
+# def allowed_users(allowed_roles=[]):
+#     def decorator(view_function):
+#         def wrapper_func(request, *args, **kwargs):
+#             group = None
+#             if request.user.groups.exists():
+#                 group = request.user.groups.all()[0].name
+#             if group in allowed_roles:
+#                 return view_function(request, *args, **kwargs)
+#             else:
+#                 return HttpResponse('Vous n\'etez pas autorisée a visiter cette page !')
+#         return wrapper_func
+#
+#     return decorator
