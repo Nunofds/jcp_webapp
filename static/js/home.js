a5ebfe4597446922cdde6e9121c073f10d229cac
Nuno Fernandes <fernandes.n.sa@outlook.fr>
2022-10-22 05:07:16 +0200

    add js for close message with information alert

---
+++ static/js/home.js
@@ -0,0 +1,4 @@
+// event to close info message
+document.getElementById("close_alert_message").addEventListener("click", () => {
+    document.querySelector(".info_alert_message").style.display = "none";
+});
